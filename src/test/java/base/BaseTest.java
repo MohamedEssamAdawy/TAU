a245b7c6500eaa52b99bb47da5986b86b89dfaae
MElborolossy <mohammedelborolossy@gmail.com>
2020-04-02 08:10:14 +0200

    Add Chapter 13 codes, Optional Exercise is to implement more EventListeners I made afterScript and test it wit JavascriptTest.

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,27 +1,33 @@
 package base;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
+import com.google.common.io.Files;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.testng.ITest;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
+import utils.EventReporter;
 import utils.WindowManger;
 
-import java.util.List;
-import java.util.concurrent.TimeUnit;
+import java.io.File;
+import java.io.IOException;
 
 public class BaseTest {
-    private WebDriver driver;
+    private EventFiringWebDriver driver;
     protected HomePage homePage;
 
     @BeforeClass
     public void setUp (){
         System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
-        driver = new ChromeDriver();
+        driver = new EventFiringWebDriver(new ChromeDriver());
+        driver.register(new EventReporter());
         //Implicit TimeOut
         //driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         goHome();
@@ -36,6 +42,20 @@ public class BaseTest {
     @AfterClass
     public void tearDown(){
         driver.quit();
+    }
+
+    @AfterMethod
+    public void recordFailure(ITestResult result){
+        if(ITestResult.FAILURE == result.getStatus()){
+            var camera = (TakesScreenshot)driver;
+            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+            try {
+                Files.move(screenshot, new File("resources/screenshots/"+result.getName()+".png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Screenshot taken: " + screenshot.getAbsolutePath());
+        }
     }
 
     public WindowManger getWindowManger(){
